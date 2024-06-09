f69dd8c73394476ac8bc0b57417440c984d2c76f
sheygs <macbookpro@sheygs-macbook.local>
2024-06-09 21:15:13 +0100

    chore(): Base endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  baseRoute(): { message: string; code: number; status: string } {
+    return this.appService.getHealth();
   }
 }
