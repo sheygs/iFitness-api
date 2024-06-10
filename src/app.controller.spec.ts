2dbd9ebee7c35c80939a731d98db76a449b1c31b
sheygs <macbookpro@sheygs-macbook.local>
2024-06-10 16:32:33 +0100

    refactor(): Reusable modules to `shared` module

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UtilitiesModule } from './shared';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [UtilitiesModule],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
